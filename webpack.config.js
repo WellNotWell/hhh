751eab708d1bdc3c5811f6455468a2b03e03c2d0
WellNotWell <olesya.gredyushko4956@gmail.com>
2024-07-03 06:51:49 +0300

    5

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+    publicPath: '/',
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
@@ -42,11 +43,7 @@ module.exports = {
     }),
   ],
   devServer: {
-    static: {
-      directory: path.join(__dirname, 'dist'),
-    },
-    compress: true,
-    port: 9000,
-    historyApiFallback: true,
+    contentBase: './dist', // Корневая папка для dev-сервера
+    historyApiFallback: true, // Для поддержки react-router
   },
 };
